e871133aaf29e0377e0cb03bdf8280a7e9b628e6
tricky <trickya23@gmail.com>
2024-04-23 16:37:53 +0000

    update hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <section className="container min-h-[calc(100vh-158px)]">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-6 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1
             className={`${permanent_marker.className} max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl`}
